6459681d3881030708ba0bb976d4ae2e584c5b3d
rubacodes <rubacodes@gmail.com>
2022-05-12 15:41:36 +0200

    importata funzione random integer

---
+++ script.js
@@ -0,0 +1,13 @@
+// Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
+// Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+// --- FUNZIONI ---
+
+//ritorna un array di numeri random tra min e mex per size volte
+function getRndInteger(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// ---MAIN ---
+console.log(getRndInteger(1,20))
